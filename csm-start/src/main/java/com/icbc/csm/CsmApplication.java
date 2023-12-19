aeceedc6c996eafaf609ca79e325292ceb7dc4d2
裴博润 <001236865@mails.icbc>
2023-12-19 21:38:33 +0800

    【feature_0113_20231219】【JIRA】【XA1-79918】提交java项目启动方式
    【分支】【feature_0113_20231129】
    【项目编号】
    【项目名称】
    【任务描述】
    【版本计划】2024年1月份版本
    ****************************************
    Reviewer:孙凤津(001345512)
    
    Change-Id: I3e598b3cdf475ac4ec3b990702709a61f1d1e4fe
    Reviewed-on: http://scm-xa.sdc.cs.icbc:8080/c/ccis/customerservicemobilerefactor/+/318752
    Tested-by: AutoReviewer <gitadm@sdc.icbc.com.cn>
    Reviewed-by: 孙凤津 <001345512@mails.icbc>

---
+++ csm-start/src/main/java/com/icbc/csm/CsmApplication.java
@@ -0,0 +1,14 @@
+package com.icbc.csm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CsmApplication {
+    public static void main(String[] args) {
+        //java项目启动方式
+        SpringApplication.run(CsmApplication.class, args);
+    }
+}
